b50dfa3b7aaea90244bf1940b9dcd8843542fa6f
Metallizer <thomas.noury@protonmail.com>
2020-03-22 13:25:57 +0100

    Added effect enum

---
+++ src/Effect.cs
@@ -0,0 +1,21 @@
+﻿namespace SixteenBitNuts
+{
+    enum PostProcessEffect
+    {
+        Glow,
+        Glitch
+    }
+
+    static class PostProcessEffectExtension
+    {
+        public static string? GetPath(this PostProcessEffect effect)
+        {
+            return effect switch
+            {
+                PostProcessEffect.Glow => "Game/effects/glow",
+                PostProcessEffect.Glitch => "Game/effects/glitch",
+                _ => null,
+            };
+        }
+    }
+}
