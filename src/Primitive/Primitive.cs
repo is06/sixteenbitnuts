a26d046b3327ca15caadd9de62463259ac5d3713
Metallizer <thomas.noury@protonmail.com>
2020-03-26 18:12:28 +0100

    Circle primitive added

---
+++ src/Primitive/Primitive.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SixteenBitNuts
+{
+    public class Primitive
+    {
+        protected readonly Texture2D pixel;
+        protected readonly Game game;
+
+        public Primitive(Game game)
+        {
+            this.game = game;
+
+            pixel = new Texture2D(game.GraphicsDevice, 1, 1);
+            pixel.SetData(new[] { Color.White });
+        }
+
+        public virtual void Draw()
+        {
+
+        }
+    }
+}
