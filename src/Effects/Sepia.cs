1ba5593461ec990d243b766562ea851c84fefbe0
Metallizer <thomas.noury@protonmail.com>
2020-03-28 11:07:52 +0100

    Sepia shader added

---
+++ src/Effects/Sepia.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace SixteenBitNuts.Effects
+{
+    public class Sepia : MainDisplayEffect
+    {
+        public Sepia(Game game) : base(game)
+        {
+            Effect = game.Content.Load<Effect>("Engine/effects/sepia");
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+    }
+}
