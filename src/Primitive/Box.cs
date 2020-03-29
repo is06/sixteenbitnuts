1840a4b45d36ad4f3e352ee62a744652d85fde88
Metallizer <thomas.noury@protonmail.com>
2020-03-29 17:59:45 +0200

    Removed thickness of box

--- src/Primitive/Box.cs
+++ src/Primitive/Box.cs
@@ -7,13 +7,11 @@ namespace SixteenBitNuts
         private readonly OldLine[] lines;
 
         public Rectangle Bounds { get; set; }
-        public int Thickness { get; set; }
         public Color Color { get; set; }
 
-        public Box(Game game, Rectangle bounds, int thickness, Color color)
+        public Box(Game game, Rectangle bounds, Color color)
         {
             Bounds = bounds;
-            Thickness = thickness;
             Color = color;
 
             lines = new OldLine[4];
@@ -31,13 +29,13 @@ namespace SixteenBitNuts
             }
 
             // top
-            lines[0].Bounds = new Rectangle(Bounds.X, Bounds.Y, Bounds.Width, Thickness);
+            lines[0].Bounds = new Rectangle(Bounds.X, Bounds.Y, Bounds.Width, 1);
             // bottom
-            lines[1].Bounds = new Rectangle(Bounds.X, Bounds.Y + Bounds.Height - Thickness, Bounds.Width, Thickness);
+            lines[1].Bounds = new Rectangle(Bounds.X, Bounds.Y + Bounds.Height - 1, Bounds.Width, 1);
             // left
-            lines[2].Bounds = new Rectangle(Bounds.X, Bounds.Y, Thickness, Bounds.Height);
+            lines[2].Bounds = new Rectangle(Bounds.X, Bounds.Y, 1, Bounds.Height);
             // right
-            lines[3].Bounds = new Rectangle(Bounds.X + Bounds.Width - Thickness, Bounds.Y, Thickness, Bounds.Height);
+            lines[3].Bounds = new Rectangle(Bounds.X + Bounds.Width - 1, Bounds.Y, 1, Bounds.Height);
         }
 
         public void Draw(Matrix transform)
