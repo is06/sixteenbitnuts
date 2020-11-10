cb02be62467c5951bb5ee70d7ff6dc217d50bbb4
Metallizer <thomas.noury@protonmail.com>
2020-11-10 11:36:19 +0100

    Can now override controls in sub player class update and set gravity incidence

--- src/Player/PlatformerPlayer.cs
+++ src/Player/PlatformerPlayer.cs
@@ -12,6 +12,7 @@ namespace SixteenBitNuts
 
     public abstract class PlatformerPlayer : Player
     {
+        public bool IsGravityEnabled { get; set; }
         public bool IsRunning { get; set; }
         public bool IsJumping { get; set; }
         public bool IsDucking { get; set; }
@@ -23,6 +24,7 @@ namespace SixteenBitNuts
         public float RunSpeed { get; protected set; }
         public float JumpForce { get; protected set; }
         public float DuckOffset { get; protected set; }
+        public bool OverrideAllControls { get; set; }
 
         public event PlatformerPlayerActionHandler? OnPerformAction;
 
@@ -35,168 +37,168 @@ namespace SixteenBitNuts
             JumpForce = -6f;
             DuckOffset = 8f;
             IsFalling = true;
+            IsGravityEnabled = true;
         }
 
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
 
-            #region Ducking
-
-            if (IsControllable)
+            if (!OverrideAllControls)
             {
-                IsDucking = false;
+                #region Ducking
 
-                if (!IsJumping && !IsFalling)
+                if (IsControllable)
                 {
-                    // Gamepad
-                    if (GamePad.GetState(PlayerIndex.One).IsButtonDown(Buttons.LeftThumbstickDown) ||
-                        GamePad.GetState(PlayerIndex.One).IsButtonDown(Buttons.DPadDown))
-                    {
-                        IsDucking = true;
-                    }
+                    IsDucking = false;
 
-                    // Keyboard
-                    if (Keyboard.GetState().IsKeyDown(Keys.Down))
+                    if (!IsJumping && !IsFalling)
                     {
-                        IsDucking = true;
+                        // Gamepad
+                        if (GamePad.GetState(PlayerIndex.One).IsButtonDown(Buttons.LeftThumbstickDown) ||
+                            GamePad.GetState(PlayerIndex.One).IsButtonDown(Buttons.DPadDown))
+                        {
+                            IsDucking = true;
+                        }
+
+                        // Keyboard
+                        if (Keyboard.GetState().IsKeyDown(Keys.Down))
+                        {
+                            IsDucking = true;
+                        }
                     }
                 }
+
+                #endregion
+
+                #region Run
+
+                if (IsControllable)
+                {
+                    IsRunning = false;
+                    velocity.X = 0;
+
+                    if (!IsDucking)
+                    {
+                        // Gamepad
+                        if (GamePad.GetState(PlayerIndex.One).IsButtonDown(Buttons.LeftThumbstickLeft) ||
+                            GamePad.GetState(PlayerIndex.One).IsButtonDown(Buttons.DPadLeft))
+                        {
+                            velocity.X = -RunSpeed;
+                            IsRunning = true;
+                            Direction = Direction.Left;
+                            if (sprite != null) sprite.Direction = Direction.Left;
+                        }
+
+                        if (GamePad.GetState(PlayerIndex.One).IsButtonDown(Buttons.LeftThumbstickRight) ||
+                            GamePad.GetState(PlayerIndex.One).IsButtonDown(Buttons.DPadRight))
+                        {
+                            velocity.X = RunSpeed;
+                            IsRunning = true;
+                            Direction = Direction.Right;
+                            if (sprite != null) sprite.Direction = Direction.Right;
+                        }
+
+                        // Keyboard
+                        if (Keyboard.GetState().IsKeyDown(Keys.Left))
+                        {
+                            velocity.X = -RunSpeed;
+                            IsRunning = true;
+                            Direction = Direction.Left;
+                            if (sprite != null) sprite.Direction = Direction.Left;
+                        }
+
+                        if (Keyboard.GetState().IsKeyDown(Keys.Right))
+                        {
+                            velocity.X = RunSpeed;
+                            IsRunning = true;
+                            Direction = Direction.Right;
+                            if (sprite != null) sprite.Direction = Direction.Right;
+                        }
+                    }
+                }
+
+                #endregion
+
+                #region Jumps
+
+                if (IsControllable)
+                {
+                    if (!IsDucking && IsTouchingTheGround && !jumpButtonPressed && Keyboard.GetState().IsKeyDown(Keys.C))
+                    {
+                        OnPerformAction?.Invoke(PlatformerPlayerActionType.Jump);
+                        velocity.Y = JumpForce;
+                        IsTouchingTheGround = false;
+                        jumpButtonPressed = true;
+                    }
+                    if (Keyboard.GetState().IsKeyUp(Keys.C))
+                    {
+                        // If the player is moving up
+                        if (velocity.Y < 0)
+                        {
+                            velocity.Y *= 0.5f;
+                        }
+                        jumpButtonPressed = false;
+                    }
+
+                    if (velocity.Y > 0)
+                    {
+                        IsJumping = false;
+                        IsFalling = true;
+                    }
+                    else if (velocity.Y < 0)
+                    {
+                        IsJumping = true;
+                        IsFalling = false;
+                    }
+                }
+
+                #endregion
+
+                #region Animations
+
+                if (sprite != null)
+                {
+                    if (IsRunning)
+                        sprite.AnimationName = "run";
+
+                    if (IsJumping)
+                        sprite.AnimationName = "jump";
+                    else if (IsFalling)
+                        sprite.AnimationName = "fall";
+                    else if (IsDucking)
+                        sprite.AnimationName = "duck";
+
+                    if (!IsRunning && !IsJumping && !IsFalling && !IsDucking)
+                        sprite.AnimationName = "idle";
+                }
+
+                #endregion
             }
-
-            #endregion
-
-            #region Run
-
-            if (IsControllable)
-            {
-                IsRunning = false;
-                velocity.X = 0;
-
-                if (!IsDucking)
-                {
-                    // Gamepad
-                    if (GamePad.GetState(PlayerIndex.One).IsButtonDown(Buttons.LeftThumbstickLeft) ||
-                        GamePad.GetState(PlayerIndex.One).IsButtonDown(Buttons.DPadLeft))
-                    {
-                        velocity.X = -RunSpeed;
-                        IsRunning = true;
-                        Direction = Direction.Left;
-                        if (sprite != null) sprite.Direction = Direction.Left;
-                    }
-
-                    if (GamePad.GetState(PlayerIndex.One).IsButtonDown(Buttons.LeftThumbstickRight) ||
-                        GamePad.GetState(PlayerIndex.One).IsButtonDown(Buttons.DPadRight))
-                    {
-                        velocity.X = RunSpeed;
-                        IsRunning = true;
-                        Direction = Direction.Right;
-                        if (sprite != null) sprite.Direction = Direction.Right;
-                    }
-
-                    // Keyboard
-                    if (Keyboard.GetState().IsKeyDown(Keys.Left))
-                    {
-                        velocity.X = -RunSpeed;
-                        IsRunning = true;
-                        Direction = Direction.Left;
-                        if (sprite != null) sprite.Direction = Direction.Left;
-                    }
-
-                    if (Keyboard.GetState().IsKeyDown(Keys.Right))
-                    {
-                        velocity.X = RunSpeed;
-                        IsRunning = true;
-                        Direction = Direction.Right;
-                        if (sprite != null) sprite.Direction = Direction.Right;
-                    }
-                }
-            }
-
-            #endregion
-
-            #region Jumps
-
-            if (IsControllable)
-            {
-                if (!IsDucking && IsTouchingTheGround && !jumpButtonPressed && Keyboard.GetState().IsKeyDown(Keys.C))
-                {
-                    OnPerformAction?.Invoke(PlatformerPlayerActionType.Jump);
-                    velocity.Y = JumpForce;
-                    IsTouchingTheGround = false;
-                    jumpButtonPressed = true;
-                }
-                if (Keyboard.GetState().IsKeyUp(Keys.C))
-                {
-                    // If the player is moving up
-                    if (velocity.Y < 0)
-                    {
-                        velocity.Y *= 0.5f;
-                    }
-                    jumpButtonPressed = false;
-                }
-
-                if (velocity.Y > 0)
-                {
-                    IsJumping = false;
-                    IsFalling = true;
-                }
-                else if (velocity.Y < 0)
-                {
-                    IsJumping = true;
-                    IsFalling = false;
-                }
-            }
-
-            #endregion
-
-            #region Animations
-
-            if (sprite != null)
-            {
-                if (IsRunning)
-                    sprite.AnimationName = "run";
-
-                if (IsJumping)
-                    sprite.AnimationName = "jump";
-                else if (IsFalling)
-                    sprite.AnimationName = "fall";
-                else if (IsDucking)
-                    sprite.AnimationName = "duck";
-
-                if (!IsRunning && !IsJumping && !IsFalling && !IsDucking)
-                    sprite.AnimationName = "idle";
-            }
-
-            #endregion
         }
 
         public override void Draw(Matrix transform)
         {
             if (sprite != null)
             {
-                var pos = DrawingPosition;
-
-                if (IsDucking)
-                {
-                    pos.Y -= DuckOffset;
-                }
-
-                sprite.Draw(position: pos, layer: 0f, transform: transform);
+                sprite.Draw(position: DrawingPosition, layer: 0f, transform: transform);
             }
         }
 
         public override void ComputePhysics()
         {
-            velocity += map.Gravity * Weight;
-
-            if (IsTouchingTheGround)
-                velocity.Y = 0;
-
-            if (IsTouchingTheCeiling)
+            if (IsGravityEnabled)
             {
-                IsTouchingTheCeiling = false;
-                velocity.Y *= -0.5f;
+                velocity += map.Gravity * Weight;
+
+                if (IsTouchingTheGround)
+                    velocity.Y = 0;
+
+                if (IsTouchingTheCeiling)
+                {
+                    IsTouchingTheCeiling = false;
+                    velocity.Y *= -0.5f;
+                }
             }
 
             base.ComputePhysics();
